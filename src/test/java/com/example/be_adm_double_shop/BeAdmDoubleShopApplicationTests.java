df72cbbe3b3ec22eac96c545effe1d01f403a501
Pham Thanh Son (FIS PBDXGHN) <sonpt51@fpt.com>
2023-10-04 22:20:31 +0700

    up

---
+++ src/test/java/com/example/be_adm_double_shop/BeAdmDoubleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_adm_double_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAdmDoubleShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
