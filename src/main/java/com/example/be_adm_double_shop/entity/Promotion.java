fb2800988c6795c8dfcb7bc15f27c7f273bc1577
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-01-10 01:25:11 +0700

    sua lai cot updated_by -> updatedBy

--- src/main/java/com/example/be_adm_double_shop/entity/Promotion.java
+++ src/main/java/com/example/be_adm_double_shop/entity/Promotion.java
@@ -33,7 +33,7 @@ public class Promotion {
     private String createdBy;
 
     @Column(name = "updated_by")
-    private String updated_by;
+    private String updatedBy;
 
     @Column(name = "created_time")
     private String createdTime;
