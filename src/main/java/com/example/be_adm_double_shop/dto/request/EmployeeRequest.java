8ccd6c9cccfbe5a4fbbde0274b6e16e3f1bb5257
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-02-22 23:41:05 +0700

    anh xa lai file sql, them ben trong service employee

---
+++ src/main/java/com/example/be_adm_double_shop/dto/request/EmployeeRequest.java
@@ -0,0 +1,31 @@
+package com.example.be_adm_double_shop.dto.request;
+
+import lombok.Data;
+
+@Data
+
+public class EmployeeRequest {
+
+    private String username;
+
+    private String fullName;
+
+    private String phone;
+
+    private String email;
+
+    private String city;
+
+    private String province;
+
+    private String district;
+
+    private Integer status;
+
+    private Integer gender;
+
+    private int page;
+
+    private int pageSize;
+
+}
--- src/main/java/com/example/be_adm_double_shop/service/EmployeeService.java
+++ src/main/java/com/example/be_adm_double_shop/service/EmployeeService.java
@@ -1,12 +1,17 @@
 package com.example.be_adm_double_shop.service;
 
+import com.example.be_adm_double_shop.dto.request.EmployeeRequest;
 import com.example.be_adm_double_shop.entity.Employee;
+
+import java.util.List;
 
 public interface EmployeeService {
 
     Employee createUser(Employee user);
 
     Employee findUserbyUsername(String username);
+
+    List<Object> getAllEmployeeByCondition(EmployeeRequest employeeRequest);
 
 
     Object createEmployee(Employee employee, String creator);
