2c5e6589112aa221006a52e9f395bf5c7cdc45bd
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-03-16 14:42:12 +0700

    fix mapping bill

---
+++ src/main/java/com/example/be_adm_double_shop/service/DetailBillService.java
@@ -0,0 +1,4 @@
+package com.example.be_adm_double_shop.service;
+
+public interface DetailBillService {
+}
--- src/main/java/com/example/be_adm_double_shop/service/impl/BillServiceImpl.java
+++ src/main/java/com/example/be_adm_double_shop/service/impl/BillServiceImpl.java
@@ -1,8 +1,32 @@
 package com.example.be_adm_double_shop.service.impl;
 
+import com.example.be_adm_double_shop.dto.request.BillRequest;
+import com.example.be_adm_double_shop.entity.DetailBill;
+import com.example.be_adm_double_shop.repository.BillRepository;
 import com.example.be_adm_double_shop.service.BillService;
+import com.example.be_adm_double_shop.service.DetailProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BillServiceImpl implements BillService {
+
+    @Autowired
+    private BillRepository billRepository;
+
+    @Autowired
+    private DetailProductService detailProductService;
+
+    @Autowired
+    private DetailBill detailBill;
+
+    @Override
+    public Object createBill(BillRequest billRequest, String creator) {
+        return null;
+    }
+
+    @Override
+    public Object updateBill(BillRequest billRequest, String creator) {
+        return null;
+    }
 }
